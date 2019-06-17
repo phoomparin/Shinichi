fbe37e009e7bcdbfc083931890b8498319cc9464
Phoomparin Mano <phoomparin@gmail.com>
2019-06-17 17:44:07 +0800

    :white_check_mark: Only have title in tests

--- test/gender.test.ts
+++ test/gender.test.ts
@@ -3,32 +3,22 @@ import {Shinichi} from '../src/shinichi'
 
 describe('Gender Strategy', () => {
   it('should derive my gender from my Thai title', async () => {
-    const target: Person = {
-      thTitle: 'นาย',
-      thFirstName: 'ภูมิปรินทร์',
-      thLastName: 'มะโน'
-    }
+    const target: Person = {thTitle: 'นาย'}
 
     const shin = new Shinichi()
     shin.want('gender')
 
     const {person: p} = await shin.searchFor(target)
-
     expect(p.gender).toBe('Male')
   })
 
   it('should derive my gender from my English title', async () => {
-    const target: Person = {
-      title: 'Mrs.',
-      firstName: 'Suthida',
-      lastName: 'Vajiralongkorn'
-    }
+    const target: Person = {title: 'Mrs.'}
 
     const shin = new Shinichi()
     shin.want('gender')
 
     const {person: p} = await shin.searchFor(target)
-
     expect(p.gender).toBe('Female')
   })
 })
