95f65427364ae63adf4010a5763baf8344cd262e
Phoomparin Mano <phoomparin@gmail.com>
2019-06-18 14:47:31 +0700

    :art: Code formatting

--- src/shinichi/compose.ts
+++ src/shinichi/compose.ts
@@ -1,17 +1,18 @@
 import {Strategy, StrategyContext, StrategyResult, StrategyState} from 'types/Strategy'
 import {Person} from 'types/Person'
 
-export const combineStrategy = (...strategies: Strategy[]): Strategy => async (person, state, ctx): Promise<StrategyResult> => {
-  for (let strategy of strategies) {
-    const result = await strategy(person, state, ctx)
-    if (!result) continue
+export const combineStrategy = (...strategies: Strategy[]): Strategy =>
+  async (person, state, ctx): Promise<StrategyResult> => {
+    for (let strategy of strategies) {
+      const result = await strategy(person, state, ctx)
+      if (!result) continue
 
-    person = result.person
-    state = result.state
+      person = result.person
+      state = result.state
+    }
+
+    return {person, state}
   }
-
-  return {person, state}
-}
 
 export async function depends(strategies: Strategy[], person: Person, state: StrategyState, ctx: StrategyContext) {
   const RootStrategy = combineStrategy(...strategies)
